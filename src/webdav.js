85ebc8c72a3d56eaca827781542c4284a50697bb
Peter Hilton <hilton@users.noreply.github.com>
2017-11-21 17:25:50 +0100

    Connect to server and parse result

--- src/webdav.js
+++ src/webdav.js
@@ -1,12 +1,30 @@
-import { Connection } from "webdav-client";
+/*
+  Fetches employee-supervisor information via WebDEV.
+
+  Usage: set the environment variables before running:
+
+  WEBDAV_USER
+  WEBDAV_PASSWORD
+  WEBDAV_URL
+  WEBDAV_PATH
+*/
+
+import { Connection, BasicAuthenticator } from "webdav-client";
 import { parseString } from "xml2js";
 
 const connection = new Connection({
-  url: "",
-  username: "",
-  password: ""
+  url: process.env.WEBDAV_URL,
+  authenticator: new BasicAuthenticator(),
+  username: process.env.WEBDAV_USER,
+  password: process.env.WEBDAV_PASSWORD
 });
 
-connection.get("/some/path", (error, content) => {
-  const goodLookingContent = parseString(content);
+connection.get(process.env.WEBDAV_PATH, (error, content) => {
+  if (error) {
+    console.error(error.toString())
+    return
+  }
+  parseString(content, (error, data) => {
+    console.log(JSON.stringify(data))
+  })
 });
