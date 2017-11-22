51eeb7c836e0e311f75cefe72d36acaba8b4c4a1
Peter Hilton <hilton@users.noreply.github.com>
2017-11-22 12:30:30 +0100

    Extract employee mappings from result

--- src/webdav.js
+++ src/webdav.js
@@ -19,12 +19,46 @@ const connection = new Connection({
   password: process.env.WEBDAV_PASSWORD
 });
 
+// Returns true given a spreadsheet row that contains an (employee, manager) mapping.
+const employeeRow = (row) => {
+  return row.Cell
+    && row.Cell.length > 1
+    && row.Cell[0].$ 
+    && row.Cell[0].$['ss:Index'] == '1' 
+    && row.Cell[0].Data
+    && row.Cell[0].Data.length > 0
+    && row.Cell[0].Data[0].$['ss:Type'] == 'String'
+    && row.Cell[0].Data[0]._ 
+    && row.Cell[0].Data[0]._ != 'Name'
+    && row.Cell[1].$ 
+    && row.Cell[1].$['ss:Index'] == '10' 
+    && row.Cell[1].Data
+    && row.Cell[1].Data.length > 0
+    && row.Cell[1].Data[0].$['ss:Type'] == 'String'
+    && row.Cell[1].Data[0]._ 
+  }
+
 connection.get(process.env.WEBDAV_PATH, (error, content) => {
   if (error) {
     console.error(error.toString())
     return
   }
   parseString(content, (error, data) => {
-    console.log(JSON.stringify(data))
+    if (error) {
+      console.error(error.toString())
+      return
+    }
+    const rows = data.Workbook.Worksheet[0].Table[0].Row
+    const employees = rows.filter(employeeRow)
+
+    // Group rows into an object whose properties map employees to supervisors
+    const supervisor = employees.reduce((result, row) => {
+      const employee = row.Cell[0].Data[0]._
+      const employeeSupervisor = row.Cell[1].Data[0]._
+      result[employee] = employeeSupervisor
+      return result
+    }, {})
+    
+    console.log(JSON.stringify(supervisor, null, ' '))
   })
 });
